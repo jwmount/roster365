59cc0912ae6ec9dfeff7f7e06965df063acbf8d7
John Mount <john@venuesoftware.com>
2013-09-11 15:45:11 -0700

    Commented out 'best_in_place' references in application.js.  No idea what these assets are or where these two references came from.  Task here is to clear asset:precompile step in push to heroku error.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require best_in_place
-//= require best_in_place.purr
+// require best_in_place
+// require best_in_place.purr
 //= require_tree .
