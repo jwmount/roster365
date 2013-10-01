65cf85c123b6b1e78586d9c2077a0157f256f501
John Mount <john@venuesoftware.com>
2013-10-01 11:49:27 -0700

    AdminUsers now works, but once again had to use params.permit! in order to avoid 'found unpermitted parameters: utf8, authenticity_token, commit'.  Unable to figure where these are coming from, guess it's Rails itself, but since these parameters are visible in the params hash AND explicitly putting them into the permitted array HAS NO EFFECT this feature appears to be totally broken.

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -36,10 +36,14 @@ ActiveAdmin.register AdminUser do
     end
   end
 
+# found unpermitted parameters: utf8, authenticity_token, commit
   controller do
     def permitted_params
-#      params.require(:admin_user).permit( :id, :email, :password, :password_confirmation, :remember_me )
-      params.require(:admin_user).permit(
+      #params.require(:admin_user).permit( :id, :email, :password, :password_confirmation, :remember_me )
+     #params.require(:admin_user).permit(
+      params.permit!
+=begin
+      params.permit(:admin_user => [ 
         :email,
         :encrypted_password,
         :reset_password_token,
@@ -49,8 +53,11 @@ ActiveAdmin.register AdminUser do
         :current_sign_in_at,
         :last_sign_in_at,
         :current_sign_in_ip,
-        :last_sign_in_ip
-        )
+        :last_sign_in_ip,
+        :password
+      ]
+    )
+=end
     end
   end  
 
