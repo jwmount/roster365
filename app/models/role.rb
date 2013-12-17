06708c65be803d895839385a09a7cb183f8c2c30
John Mount <john@venuesoftware.com>
2013-12-17 08:52:26 -0800

    vendor compiled assets

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,6 @@
 class Role < ActiveRecord::Base
   
-  has_and_belongs_to_many :users, :class_name => "AdminUser", :join_table => "roles_users", :association_foreign_key => "user_id"
+  has_and_belongs_to_many :users#, :class_name => "AdminUser", :join_table => "roles_users", :association_foreign_key => "user_id"
   
   validates_presence_of :name
 
