38a4a4b051665c1823b0a0a5a4e6790eac89cf9f
John Mount <john@venuesoftware.com>
2013-12-17 21:43:36 -0800

    v1.2.41.  Added AbilityHelper module to application_controller.rb.  First step to implementing authorization based on CanCan.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-#require 'debugger'
+require 'debugger'
 
 def error_panel( f )
   if f.object.errors.size >= 1  
@@ -49,7 +49,40 @@ class ActiveAdmin::Views::Pages::Base < Arbre::HTML::Document
 
 end
 
+#
+#  M O D U L E 
+#
+module AbilityHelper
 
+  class Ability
+    include CanCan::Ability
+ 
+    def initialize(user)
+      user ||= User.new # guest user
+ 
+      if user.role? :super_admin
+        can :manage, :all
+      elsif user.role? :product_admin
+        can :manage, [Product, Asset, Issue]
+      elsif user.role? :product_team
+        can :read, [Product, Asset]
+        # manage products, assets he owns
+        can :manage, Product do |product|
+          product.try(:owner) == user
+        end
+        can :manage, Asset do |asset|
+          asset.assetable.try(:owner) == user
+        end
+      end
+    end
+
+  end
+  
+end
+
+#
+#  M O D U L E 
+#
 module ActiveAdmin
 
   class Resource
--- config/boot.rb
+++ config/boot.rb
@@ -5,6 +5,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-ENV['VERSION'] = "v1.2.40. &copy Copyright Venue Software Corporation, 2010 - 2013. "
+ENV['VERSION'] = "v1.2.41. &copy Copyright Venue Software Corporation, 2010 - 2013. "
 ENV['LICENSEE'] = 'Valley Farm Transport'
 LICENSEE = ENV['LICENSEE']
