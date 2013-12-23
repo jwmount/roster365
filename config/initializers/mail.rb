8943103723be9f2b7a0a7d29c7f8ef1e9b275c8b
John Mount <john@venuesoftware.com>
2013-12-23 10:29:43 -0800

    SMTP manual config to use :domain 'ninefold.apps.com'

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -5,6 +5,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+  #:domain         => 'heroku.com' 
+  :domain         => 'ninefold.apps.com'
 }
 ActionMailer::Base.delivery_method = :smtp
