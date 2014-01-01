b9532422ea57c8dcab3bd0c4a0624e35a22018f7
John Mount <john@venuesoftware.com>
2013-12-31 18:08:12 -0800

    v1.2.49.  Repeated to remove require 'debugger' and SAVE it this time.

--- config/boot.rb
+++ config/boot.rb
@@ -5,7 +5,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-ENV['VERSION'] = "v1.2.48. &copy Copyright Venue Software Corporation, 2010 - 2013. "
+ENV['VERSION'] = "v1.2.49. &copy Copyright Venue Software Corporation, 2010 - 2013. "
 ENV['LICENSEE'] = 'Valley Farm Transport'
 ENV['LICENSEE_LOB'] = "Farm Transport"
 ENV['LICENSEE_URL'] = "www.ValleyTransport.com"
