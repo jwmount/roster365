2cbe4246ee080ff7da8a502f8982bfa883fdda2d
John Mount <john@venuesoftware.com>
2014-01-02 21:45:03 -0800

    v1.2.51.  Adjustments and additions to seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,7 @@ end
 #
 # T I P  S I T E S
 #
+# "Blue Line Transfer" is a tip, put it in here!
 [
   [ "ABC Tip", 1, 10.00, 'High'],
   [ "Marin Recycling Center", 2, 15.00, 'None'],
@@ -47,7 +48,9 @@ certificate_list = [
   [ 'VIN', 'By inspection.', false, false, true, false, false ],
   [ 'Aluminum body', '', false, false, true, false, false ],
   [ 'Mud flaps', '', false, false, true, false, false ],
-  [ 'Green Flag', "No toxic or bad stuff", false, false, false, false, true ]
+  [ 'Green Flag', "No toxic or bad stuff", false, false, false, false, true ],
+  [ "WBE", "CA Womman Owned Business", false, true, false, false, false],
+  [ "SDB", "State & Federal Small Disadvantaged Business", false, true, false, false, false]
 ]
 certificate_list.each do |name, description, for_person, for_company, for_equipment, for_location, active |
   Certificate.create!( name: name, description: description, for_person: for_person, for_company: for_company, 
@@ -80,93 +83,99 @@ companies_list = [
     "person"     => { first_name: "Account", last_name: "Representative", title: "Rep"},
     "identifier" => { name: "Office", value: "415 555-1212"}
     }, 
-  { "company"    => { name: "American Debris Box Service Inc.", line_of_business: "Containers delivered and removed", url: ""},
-    "address"    => {},
+  { "company"    => { name: "1st Logistics", line_of_business: "Over-the-road transportation services", url: "www.truckcompaniesin.com/dot/1175905/"},
+    "address"    => { street_address: "2001 Meyer Way", city: "Fairfield", state: "CA", post_code: "94533" },
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "(707) 853-6712"}
     }, 
-  { "company"    => { name: "Lawson Drayage, Inc.", line_of_business: "Cartage, Local Houl Freight Carrying Service", url: ""},
-    "address"    => {},
+  { "company"    => { name: "American Debris Box Service Inc.", line_of_business: "Containers delivered and removed", url: "americandebrisbox.com/"},
+    "address"    => { street_address: "PO Box 3187", city: "Half Moon Bay", state: "CA", post_code: "94019" },
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
-    }, 
-  { "company"    => { name: "Waste Management, Inc.", line_of_business: "Waste Disposal & Removal Service", url: ""},
-    "address"    => {},
-    "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
-    }, 
-  { "company"    => { name: "Reliable Crane & Rigging", line_of_business: "Carting, Hoist Service & Rental, Machinery Moving & Erecting Service", url: ""},
-    "address"    => {},
-    "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
-    }, 
-  { "company"    => { name: "Foothill Disposal Co. Inc.", line_of_business: "Cartage Service", url: ""},
-    "address"    => {},
-    "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
-    }, 
-  { "company"    => { name: "BWRS", line_of_business: "Debris box, compactors, recycling", url: ""},
-    "address"    => {},
-    "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
-    }, 
-  { "company"    => { name: "Pleasanton Garbage Serivice, Inc.", line_of_business: "Waste Transport Service", url: ""},
-    "address"    => {},
-    "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
-    }, 
-  { "company"    => { name: "Dillard Trucking, Inc.", line_of_business: "Hazardous Waste Transport", url: ""},
-    "address"    => {},
-    "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "866-332-7471"}
     }, 
   { "company"    => { name: "Blue Line Transfer", line_of_business: "Cartage, waste removal & recycling service", url: ""},
+    "address"    => { street_address: "500 E. James Court", city: "South San Francisco", state: "CA", post_code: "94080"},
+    "person"     => { last_name: "t.b.d."},
+    "identifier" => { name: "Main Office", value: "t.b.d."}
+    }, 
+  { "company"    => { name: "Lawson Drayage, Inc.", line_of_business: "Machinery Moving, Rigging, & Heavy Transportation for any Industry",
+                      url: "lawsoninc.com"},
+    "address"    => { street_address: "3402 Enterprise Avenue", city: "Hayward", state: "CA", post_code: "94545"},
+    "person"     => { last_name: "t.b.d."},
+    "identifier" => { name: "Main Office", value: "510-785-5100 "}
+    }, 
+  { "company"    => { name: "Waste Management, Inc.", line_of_business: "Waste Disposal & Removal Service", url: "www.wm.com"},
     "address"    => {},
     "person"     => { last_name: "t.b.d."},
     "identifier" => { name: "Main Office", value: "t.b.d."}
     }, 
-  { "company"    => { name: "Gan-Tran Ltd.", line_of_business: "Cartage Services", url: ""},
+  { "company"    => { name: "Reliable Crane & Rigging", line_of_business: "Carting, Hoist Service & Rental, Machinery Moving & Erecting Service", url: " www.reliablecrane.com"},
+    "address"    => {},
+    "person"     => { last_name: "t.b.d."},
+    "identifier" => { name: "Main Office", value: "800 222-7263"}
+    }, 
+  { "company"    => { name: "Foothill Disposal Co. Inc.", line_of_business: "Cartage Service", url: "www.norcalwaste.com"},
     "address"    => {},
     "person"     => { last_name: "t.b.d."},
     "identifier" => { name: "Main Office", value: "t.b.d."}
+    }, 
+  { "company"    => { name: "BWRS", line_of_business: "Debris box, compactors, recycling", url: "www.bwrs.com/"},
+    "address"    => {},
+    "person"     => { last_name: "t.b.d."},
+    "identifier" => { name: "Main Office", value: "t.b.d."}
+    }, 
+  { "company"    => { name: "Pleasanton Garbage Serivice, Inc.", line_of_business: "Waste Transport Service", url: "www.pleasantongarbageservices.com"},
+    "address"    => { street_address: "3110 Busch Road", city: "Pleasanton", state: "CA", post_code: "94566"},
+    "person"     => { last_name: "t.b.d."},
+    "identifier" => { name: "Main Office", value: "(925) 846-2042"}
+    }, 
+  { "company"    => { name: "Dillard Trucking, Inc.", line_of_business: "Hazardous Waste Transport", url: "dillardenv.com"},
+    "address"    => { street_address: "3120 Camino Diablo Road, PO Box 579", city: "Byron", state: "CA", post_code: "94514"},
+    "person"     => { last_name: "t.b.d."},
+    "identifier" => { name: "Main Office", value: "925 634-6850"}
+    }, 
+  { "company"    => { name: "Gan-Trans Ltd.", line_of_business: "Cartage Services (not local)", url: ""},
+    "address"    => { street_address: "800 Carden St.", city: "San Leandro", state: "CA", post_code: "94577"},
+    "person"     => { last_name: "t.b.d."},
+    "identifier" => { name: "Main Office", value: "510 357-3100"}
     }, 
   { "company"    => { name: "Precision Crane Service", line_of_business: "Crane & Freight Carrier Service", url: ""},
-    "address"    => {},
+    "address"    => { street_address: "", city: "San Rafael", state: "CA", post_code: "94901"},
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "415 457-0702"}
     }, 
-  { "company"    => { name: "East Bay Sanitary Co., Inc.", line_of_business: "Cartage, Waste Disposal & Removing Service", url: ""},
-    "address"    => {},
+  { "company"    => { name: "East Bay Sanitary Co., Inc.", line_of_business: "Cartage, Waste Disposal & Removing Service", url: "www.ebsan.com"},
+    "address"    => { street_address: "PO Box 1316, 1432 Kearny St.", city: "El Cerrito", state: "CA", post_code: "94530"},
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "510.237.4321"}
     }, 
   { "company"    => { name: "Vallejo Garbage Service", line_of_business: "Cartages Service", url: ""},
-    "address"    => {},
+    "address"    => { street_address: "2021 Broadway St.", city: "Vallejo", state: "CA", post_code: "94589"},
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "707 552-3110"}
     }, 
-  { "company"    => { name: "Fred Haines & Company", line_of_business: "Cartage Service", url: ""},
-    "address"    => {},
-    "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+  { "company"    => { name: "Fred Haines & Company", line_of_business: "Cartage Service", url: "hainesinc.com"},
+    "address"    => { street_address: "40434 Brickyard Dr", city: "Madera", state: "CA", post_code: "93636"},
+    "person"     => { first_name: "Jason", last_name: "Haines", title: "Dispatcher"},
+    "identifier" => { name: "Main Office", value: "(559) 451-0100"}
     }, 
-  { "company"    => { name: "Specialty Solid Waste & Recycling", line_of_business: "Waste Handling, Cartage, Waste Disposal Service", url: ""},
-    "address"    => {},
+  { "company"    => { name: "Specialty Solid Waste & Recycling", line_of_business: "Waste Handling, Cartage, Waste Disposal Service", url: "www.sswr.com"},
+    "address"    => { street_address: "3355 Thomas Road", city: "Santa Clara", state: "CA", post_code: "95054"},
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "408.565.9900"}
     }, 
   { "company"    => { name: "Tight Access Excavation", line_of_business: "Residential - Commercial", url: ""},
-    "address"    => {},
+    "address"    => { street_address: "8140 SE 51ST AVE", city: "Portland", state: "OR", post_code: "97206"},
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "(503) 287-4836"}
     }, 
-  { "company"    => { name: "Hazel Construction", line_of_business: "Residential Construction", url: ""},
-    "address"    => {},
+  { "company"    => { name: "Hazel Construction", line_of_business: "Residential Construction", url: "hazel-construction.hub.biz/"},
+    "address"    => { street_address: "70 Greenfield Ave.", city: "San Anselmo", state: "CA", post_code: ""},
     "person"     => { last_name: "t.b.d."},
-    "identifier" => { name: "Main Office", value: "t.b.d."}
+    "identifier" => { name: "Main Office", value: "415 457-2086"}
     }, 
   { "company"    => { name: "TMT Industries", line_of_business: "Residential Construction", url: ""},
-    "address"    => {},
+    "address"    => { street_address: "14859 Whittram Ave.", city: "Fontana", state: "CA", post_code: "92335"},
     "person"     => { last_name: "t.b.d."},
     "identifier" => { name: "Main Office", value: "t.b.d."}
     }, 
