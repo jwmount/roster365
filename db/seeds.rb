94a10e367c438bade876e6c45659e2de53cdffdb
John Mount <john@venuesoftware.com>
2014-01-04 18:34:03 -0800

    v.1.2.53.  Trivial for deploy to Ninefold.  Fails on Company exists, in rescue blaock.

--- db/seeds.rb
+++ db/seeds.rb
@@ -506,6 +506,215 @@ projects_list = [
 
 ]
 
+projects_list.each do |model| 
+  begin
+    @company = Company.create!( model["company"] )
+    @company.addresses.create!( model["address"] )
+    @company.identifiers.create!( model["identifier"] )
+    puts @company.name + " -- Created."
+  rescue
+    #company_association = Company.where( name: model["company"][:name])
+    company_association = Company.where( model["company"] )
+    @company = company_association[0]
+  end
+  @project = @company.projects.create!( model["project"])
+  @project.addresses.create!( model["projAddr"])
+  @projmgr = @company.people.first
+  puts "#{model} -- Created."
+end
+
+
+#
+# Material types  
+#
+[
+  "Clean Fill",
+  "Clean Compactable Fill",
+  "General Fill",
+  "Shale",
+  "Clay Fill",
+  "Spoil",
+  "Unsuitable Rocky Fill",
+  "Unsuitable Clay Fill",
+  "Unsuitable Fill",
+  "Unsuitable Spoil",
+  "Unsuitable Material",
+  "Top Soil ",
+  "Screened Top Soil",
+  "Unscreened Top Soil",
+  "Clay Topsoil",
+  "Mulch Fine",
+  "Mulch Coarse",
+  "Mulch Dirty",
+  "Green Waste",
+  "Green Waste Tree Stumps, Palms or Root Balls",
+  "C&D Waste",
+  "C&D Waste 20%  and under Concrete",
+  "C&D Waste 21% and over Concrete",
+  "Timber ",
+  "Steel ",
+  "Rubbish",
+  "Clean Concrete",
+  "Dirty Concrete",
+  "Concrete Light Grade",
+  "Concrete Heavy Grade",
+  "Contaminated Soil",
+  "Contaminated Soil Heavy Metals",
+  "Contaminated Soil Petroleum ",
+  "Asbestos contaminated Soil",
+  "Asbestos ",
+  "Acid Sulphate Fill",
+  "Acid Sulphate Fill Over Optimum",
+  "Lime Treated Acid Sulphate Fill",
+  "Sand",
+  "Clean Sand Free of contaminates",
+  "Coarse Sand",
+  "Fine Sand",
+  "White Sand",
+  "Washed Sand",
+  "Beach Sand",
+  "Coal stone",
+  "Over Optimum Fill",
+  "Over Burden",
+  "CBR 0 to 4 Material",
+  "CBR 5 to 8 Material",
+  "CBR 8 to 10 Material",
+  "CBR 10+ Material",
+  "CBR 15+ Material",
+  "CBR 20+ Material",
+  "CBR 30+ Material",
+  "CBR 40+ Material",
+  "Select Fill",
+  "Select Fill ( Unscreened Deco)",
+  "Select Fill ( Screened Deco)",
+  "Select Fill ( Sandstone Product)",
+  "Select Fill ( Coal Stone)",
+  "Bitumen",
+  "Cold Mix Asphalt",
+  "Hot Mix Asphalt",
+  "Road Base",
+  "Crusher Dust, Concrete",
+  "5mm Crushed Concrete",
+  "10mm Crushed Concrete",
+  "20mm Crushed Concrete",
+  "40mm Crushed Concrete",
+  "70mm Crushed Concrete",
+  "Crusher Dust",
+  "5mm Crushed Rock",
+  "10mm Crushed Rock",
+  "20mm Crushed Rock",
+  "40mm Crushed Rock",
+  "70mm Crushed Rock",
+  "150mm Crushed Rock",
+  "300mm Minus Crushed Rock",
+  "Shot Rock",
+  "Large Rock, Boulders",
+  "2.5 Gravel ",
+  "2.3 Gravel,CBR 45",
+  "2.1 Gravel, CBR 80",
+  "Potable Water",
+  "Non Potable Water",
+  "Recycled Water",
+  "Indurated Sand",
+  "Rock Institute",
+  "As Directed",
+  "Sandy Fill"
+].each do |name|
+  material = Material.create!( name: name)
+end
+
+# Conditions
+# Fire Ants verbiage text is composed at run time.
+[
+ [
+   "Fire Ants",
+   "If fire ants are present this quote includes that cost.",
+   "Required"
+ ],
+ [
+   "Client Signature",
+   "Note:  A REPRESENTATIVE FROM CLIENT CONTRACTORS IS REQUIRED TO SIGN ALL INVOICES.",
+   "Required"
+ ],
+ [
+   "Standard Contract Terms for Load Cart and Dispose",
+   "Load, Cart and Dispose Conditions: 9 hr working day. Minimum delays on site to facilitate quick entry and exit. Should delays occur due to no fault of the subcontractor, the charge will revert to current hourly hire rates. Any subsequent charges e.g. tip fees, tolls, etc will be charged individually. The Client agrees to provide exclusive access to the total volume of materials on site as quoted above. The Client is responsible for all supervision, direction and project management on the site. Roster365 (AUST) Pty Ltd will provide a 20-30T Excavator to load only, under the supervision and direction of the Client or their authorised representative. The client agrees to indemnify Roster365 (AUST) Pty Ltd from any damage claims in respect to injury of person/s or damage to property and/or utilities resultant from all excavation and cartage works provided. The client is responsible for all sediment control, traffic control & dust suppression as required. All material rates are quoted in loose cubic meters or tons. All quotes are subject to material acceptance by Import Site. All prices are based on statutory legal loads. Material to be in accordance with EPA/DPI guidelines. Quotation is valid for 30 days.",
+   "Load, Cart and Dispose"
+ ],
+ [
+   "Standard Contract Terms for Export",
+   "9 hr working day. Minimum delays on site to facilitate quick entry, loading (minimum 20 ton excavator loading) and exit. Should delays occur due to no fault of the subcontractor, the charge will revert to current hourly hire rates. Any subsequent charges e.g. tip fees, tolls, etc will be charged individually. The quoted price is based on a maximum of five (5) minutes to load each Tandem, Truck & Trailer or Semi Tipper. All material rates are quoted in loose cubic meters or tonnes. All quotes are subject to material acceptance by Import Site. All prices are based on statutory legal loads. Material to be in accordance with EPA/DPI guidelines. Quotation is valid for 30 days.",
+   "Export"
+ ],
+ [
+   "Standard Contract Terms for Import",
+   "Import Conditions: 9 hr working day. Minimum delays on site to facilitate quick entry, unloading and exit. Should delays occur due to no fault of the subcontractor, the charge will revert to current hourly hire rates. Any subsequent charges e.g. tip fees, tolls, etc will be charged individually. The quoted price is based on a maximum of five (5) minutes to unload each Tandem, Truck & Trailer or Semi Tipper. All material rates quoted in loose cubic meters or tonnes. All prices are based on statutory legal loads. Material to be in accordance with EPA & DPI guidelines. Quotation is valid for 30 days.",
+   "Import"
+ ],
+ [
+   "Standard Contract Terms for Cart Only",
+   "Cart Only Conditions: 9 hr working day. Minimum delays on site to facilitate quick entry, loading (minimum 20 tonne excavator loading), unloading and exit. Should delays occur due to no fault of the subcontractor, the charge will revert to current hourly hire rates. Any subsequent charges e.g. tip fees, tolls, etc will be charged individually. The quoted price is based on a maximum of five (5) minutes to load & unload each Tandem, Truck & Trailer or Semi Tipper. Material quantity quoted in loose cubic meters or tonnes. All prices are based on statutory legal loads. Material to be in accordance with EPA &DPI guidelines. Quotation is valid for 30 days.",
+   "Cart Only"
+ ],
+ [
+   "Budget Price Only",
+   "This price has been prepared using current market options and conditions. The price, options and conditions may differ at the time of works.",
+   "Budget Price"
+ ],
+ [
+   "Hourly Hire",
+   "Minimum 4 hours. 2 hour cancellation fee. Any subsequent charges e.g. tip fees, tolls, etc will be charged individually.  All prices are based on statutory legal loads. Minimum of half an hour travel time will be charged on all trucks supplied. Night works will incur additional 20% surcharge. Material to be in accordance with EPA/DPI guidelines. Quotation is valid for 30 days.",
+   "Hourly Hire"
+ ],
+ [
+   "Machine Hire - Wet",
+   "Machine Wet Hire Conditions: Minimum 4 hours. 2 hour cancellation fee. Any subsequent charges e.g. float fees, establishment costs etc will be charged individually. Night works will incur additional 20% surcharge. Material to be in accordance with EPA & DPI guidelines. Quotation is valid for 30 days. A two way float applies to machine hire 4 days and under. Ground engaging Tools (ripper boots, cutting edges etc) will be charged individually.",
+   "Machine Hire - Wet"
+ ],
+ [
+   "Machine Hire - Dry, Uninsured",
+   "Maximum of 40hrs per week. The Hirer is responsible for theft, loss and damage to plant and its attached tools and accessories whilst on hire and the costs of replacement or repairs to such will be charged to the Hirer. No damage waiver insurance has been taken out by hirer with any damage costs to be paid by hirer. Stand down rates do not apply to Dry Hire unless agreed in writing. Float Fees, Tolls etc will be charged individually. A two way float applies to machine hire 4 days and under. Ground engaging Tools (ripper boots, cutting edges etc) will be charged individually.",
+   "Machine Hire - Dry, Uninsured"
+ ],
+ [
+   "Machine Hire - Dry, Insured",
+   "Machine Dry Hire Conditions: Maximum of 40hrs per week.  Stand down rates do not apply to Dry Hire unless agreed in writing. Float Fees, Tolls etc will be charged individually. The Supplier offers protection against damage or malicious damage by third parties. This waiver is in the form of a 12% Surcharge on the invoiced hire. An Excess of $600.00 per item or 12% of quoted price, whichever is the higher will apply. This Property Damage Waiver does not cover Burglary or Theft of the Equipment. The Hirer is responsible for theft, loss and damage to plant and its attached tools and accessories whilst on hire and the costs of replacement or repairs to such will be charged to the Hirer. A two way float applies to machine hire 4 days and under. Ground engaging Tools (ripper boots, cutting edges etc) will be charged individually.",
+   "Machine Hire - Dry, Insured"
+ ],
+ [
+   "Free Tip - Private Customers",
+   "Conditions: Material is to be supplied and placed as directed, at the time of tipping, by the property owner or their representative. If no representative is available, all effort will be made to ensure material is placed and spread in a previously designated, and owner directed, area. roster365 has been requested to arrange supply of material to the above address by the property owner or their representative. roster365 does not take responsibility for material tipped by others. The property owner confirms that this project has the relevant Government or Council approvals. ",
+   "Free Tip - Private Customers"
+ ],
+ [
+   "COD, Credit Card Accounts",
+   "Credit Card Conditions: A 2% credit card surcharge applies to payments made by Visa or Mastercard. Roster365 does not accept American Express, Diners Club or Bankcard payments.",
+   "COD, Credit Card Accounts"
+ ],
+].each do |condition|
+  Condition.create!(:name => condition[0], :verbiage => condition[1], :indication => condition[2], change_approved_at: Date.today)
+end
+
+roles_list = %w[ admin bookeeper driver guest management operations sales superadmin ]
+roles_list.each do |role|
+  Role.create!(name: role)
+end
+
+# Create users (roles not implemented yet, MUST be chosen from roles_list)
+user_list = [
+  ['admin@venuesoftware.com', 'roster365', 1],
+  ['bookeeper@venuesoftware.com', 'bookeeper', 2],
+  ['driver@venuesoftware.com', 'driver', 3],
+  ['guest@venuesoftware.com', 'roster365', 4],
+  ['manager@venuesoftware.com', 'manager', 5],
+  ['dispatcher@venuesoftware.com', "dispatcher", 6],
+  ['Sales@venuesoftware.com', 'roster365', 7],
+  ['john@venuesoftware.com', 'roster365', 8]
+  ]
+user_list.each do |email, password, role|  
+  AdminUser.create!( email: email, password: password, password_confirmation: password, role_id: role)
+  Rails::logger.info( "*-*-*-*-* Created user #{email}, pswd: #{password.slice(0..2)}, role: #{role}" )
+end
 
 #
 # W R A P U P
