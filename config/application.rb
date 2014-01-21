a8f74b69f378310118e039412c7ae59c1712e6e9
John Mount <john@venuesoftware.com>
2014-01-21 11:04:13 -0800

    Ninefold deploy.  Still not working.  'standard_exception_handling' application.rb 165.   There IS NO line 165 in that file.  Did change I18n.enforce_available_locales to config.*.  Trying that.

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Roster365
     # Brisbane, Australia is UTC+10 --jwm
     # config.time_zone = UTC+10   #'Brisbane'
 
-    I18n.enforce_available_locales = true
+    config.I18n.enforce_available_locales = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
