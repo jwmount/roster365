0936f66b2f7f36ac841a4c1629cdc6f312edc9ea
John W. Mount <john@venuesoftware.com>
2014-09-10 15:23:44 -0700

    Used rake db:create:all and now rails s -e production works.  So pushing this version with redeploy hook set to on on host.

--- db/migrate/20140123190741_create_admin_users.rb
+++ db/migrate/20140123190741_create_admin_users.rb
@@ -1,20 +1,19 @@
 class CreateAdminUsers < ActiveRecord::Migration
   def change
-  create_table "admin_users", force: true do |t|
-    t.integer  "role_id",                             null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
+    create_table "admin_users", force: true do |t|
+      t.integer  "role_id",                             null: false
+      t.string   "email",                  default: "", null: false
+      t.string   "encrypted_password",     default: "", null: false
+      t.string   "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.integer  "sign_in_count",          default: 0
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.string   "current_sign_in_ip"
+      t.string   "last_sign_in_ip"
+      t.datetime "created_at",                          null: false
+      t.datetime "updated_at",                          null: false
+    end
   end
 end
