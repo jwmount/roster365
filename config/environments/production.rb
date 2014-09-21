88db0925d08db88915e84f351aeaaf88675203e2
John W. Mount <john@venuesoftware.com>
2014-09-20 19:36:37 -0700

    WIP Deployment.  serve_static_assets=false so NF will recompile them.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Roster365::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   # If true, Ninefold will serve assets without recompiling them
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
