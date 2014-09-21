df4a502a35219a6f43abc0da155ef298aaab0558
John W. Mount <john@venuesoftware.com>
2014-09-20 17:31:50 -0700

    WIP deployment.  Set serve_static_assets=true so Ninefold will use ones from Selene.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,8 @@ Roster365::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # If true, Ninefold will serve assets without recompiling them
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
