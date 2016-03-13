ea9a76ecbb47d5cbbf8a4dbffba188dfc27e43ed
John W. Mount <john@venuesoftware.com>
2016-03-13 09:54:52 -0700

    updated footer version label

--- config/boot.rb
+++ config/boot.rb
@@ -6,7 +6,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
  # X is the major version, Y is the minor version, and Z is the patch 
-ENV['VERSION'] = "v1.3.1.2  &copy Copyright Venue Software Corporation, 2010 - 2015. "
+ENV['VERSION'] = "v1.3.1.3, 4.1.6@r2.2.2  &copy Copyright Venue Software Corporation, 2010 - 2016. "
 
 ENV['LICENSEE'] = "General Demo Service"
 
