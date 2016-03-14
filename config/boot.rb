91661b6184fa4abb513de2f6d0658f7d54596612
John W. Mount <john@venuesoftware.com>
2016-03-14 13:47:01 -0700

    v1.3.1.6 Companies and Jobs in dashboard again.  Appears they were timing out which is a configuration problem.  Added select()'s to reduce seek latency is all.

--- config/boot.rb
+++ config/boot.rb
@@ -6,7 +6,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
  # X is the major version, Y is the minor version, and Z is the patch 
-ENV['VERSION'] = "v1.3.1.5, on 4.1.6@r2.2.2  &copy Copyright Venue Software Corporation, 2010 - 2016. "
+ENV['VERSION'] = "v1.3.1.6, on 4.1.6@r2.2.2  &copy Copyright Venue Software Corporation, 2010 - 2016. "
 
 ENV['LICENSEE'] = "General Demo Service"
 
