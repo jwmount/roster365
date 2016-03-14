28b341fbca0ce9bba598eddbea55d3be7490154d
John W. Mount <john@venuesoftware.com>
2016-03-14 11:21:07 -0700

    single line in dashboard.rb

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,5 +1,6 @@
 #ActiveAdmin::Dashboards.build do
 
+  h3 There seems to be a problem with our dashboard... 
   # Define your dashboard sections here. Each block will be
   # rendered on the dashboard in the context of the view. So just
   # return the content which you would like to display.
